4ae7576318cee27fa7962e38d4389bd2f106eb1f
emomchilov <emomchilov@captechconsulting.com>
2025-02-07 14:44:24 -0600

    Initial Commit

---
+++ Pilaura/PilauraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PilauraApp.swift
+//  Pilaura
+//
+//  Created by Eden Momchilov on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PilauraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
