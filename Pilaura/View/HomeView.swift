8b363023e2a8197753490654f59803dfd13f7103
emomchilov <emomchilov@captechconsulting.com>
2025-02-08 17:37:08 -0600

    restore home view

--- Pilaura/View/HomeView.swift
+++ Pilaura/View/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  Pilaura
 //
 //  Created by Eden Momchilov on 2/7/25.
@@ -7,27 +7,20 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    private var networking = Networking()
+struct HomeView: View {
+    @ObservedObject var networkingModel = NetworkingModel.shared
     var body: some View {
         VStack {
-        // check if user is authenticated
-        // if not, show log in view
-        
-        
-        // if authenticated, show playlist view
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-                .onTapGesture {
-                    networking.authorize()
-                }
+            // TODO: Add a loading indicator or launch screen while checking for inital token
+            if networkingModel.appRemote.isConnected {
+                PlaylistGalleryView()
+            } else {
+                AuthenticationView()
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
