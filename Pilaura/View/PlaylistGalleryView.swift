dabb587d4865406fb9a1b2a810996ad99ab00283
emomchilov <emomchilov@captechconsulting.com>
2025-02-14 15:56:18 -0500

    add networking to pull playlist data + initial playlist UI view

--- Pilaura/View/PlaylistGalleryView.swift
+++ Pilaura/View/PlaylistGalleryView.swift
@@ -8,11 +8,68 @@
 import SwiftUI
 
 struct PlaylistGalleryView: View {
+    let networkingModel = NetworkingModel.shared
+    @State var playlists: [Playlist]?
+    @State private var displayName: String = ""
+    @State private var isLoading: Bool = true
+
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if let playlists = playlists {
+                ScrollView {
+                    LazyVGrid(columns: columns) {
+                        ForEach(playlists) { playlist in
+                            playlistView(playlist: playlist)
+                        }
+                    }
+                }
+            } else {
+                ProgressView("Loading Playlists...")
+            }
+        }
+        .task {
+            await loadSpotifyData()
+        }
+    }
+    
+    private func playlistView(playlist: Playlist) -> some View {
+        VStack {
+            // TODO: Just need the first image, could refactor how this is decoded?
+            if let url = playlist.images.first?.url {
+                AsyncImage(url: URL(string: url)!) { image in
+                    image.resizable()
+                        .scaledToFill()
+                } placeholder: {
+                    ProgressView()
+                }
+                Text(playlist.name)
+                    .font(.largeTitle)
+            }
+        }
+        .padding(20)
+    }
+    
+    private func loadSpotifyData() async {
+        do {
+            let userProfile = try await networkingModel.fetchUserProfile()
+            displayName = userProfile.display_name
+            let userPlaylists = try await networkingModel.fetchPlaylists(for: userProfile.id)
+            playlists = userPlaylists
+            isLoading = false
+        } catch {
+            print("Error fetching Spotify data: \(error)")
+            isLoading = false
+        }
     }
 }
 
 #Preview {
-    PlaylistGalleryView()
+    PlaylistGalleryView(playlists: Playlist.listOfMockPlaylists)
 }
